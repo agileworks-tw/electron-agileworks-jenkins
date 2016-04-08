64a3a040b58ff0a1b47458994a7d6c0e4ceb196e
lyhcode <lyhcode@gmail.com>
2016-04-08 13:20:51 +0800

    add local

--- main.js
+++ main.js
@@ -12,13 +12,16 @@ let mainWindow;
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1280, height: 720, minWidth: 800, minHeight: 600, "node-integration": false});
+
+  mainWindow.maximize();
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  //mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('http://agileworks.tw/jenkins/');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
